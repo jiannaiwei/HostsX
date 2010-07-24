924f4097eeba820376b0e3334e4460235c014ca4
vokins <vokins@gmail.com>
2010-07-24 05:30:41 +0000

    减少升级时的无用提示信息
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@358 2448481a-0d61-11df-8457-13bc90d6e451

--- g/up.bat
+++ g/up.bat
@@ -4,17 +4,20 @@ mode con: cols=50 lines=16
 color 5f
 if exist HostsTool.bat del HostsTool.bat>nul 2>nul
 copy down\HostsTool.bat HostsTool.bat>nul 2>nul
-echo 正在安装IPv6服务：
-ipv6 install>nul 2>nul
-echo 正在开启Ipv6相关支持：
+echo 关闭DNS client服务，以加快DNS解析速度;
+echo 正在安装Ipv6协议及相关支持,以使Ipv6可用！
+echo 第一次安装Ipv6时请稍候...
 netsh interface ipv6 6to4 set state disabled>nul 2>nul
 netsh interface ipv6 set teredo enterpriseclient teredo.ipv6.microsoft.com 60 34567>nul 2>nul
 netsh interface ipv6 6to4 set state enabled>nul 2>nul
 netsh int ipv6 6to4 set relay 6to4.ipv6.microsoft.com>nul 2>nul
-sc config iphlpsvc start= auto >nul 2>nul
-net start iphlpsvc >nul 2>nul
+sc config iphlpsvc start= auto>nul 2>nul
+net start iphlpsvc>nul 2>nul
 netsh interface ipv6 6to4 set state disabled>nul 2>nul
 netsh interface ipv6 set teredo enterpriseclient teredo.ipv6.microsoft.com 60 34567>nul 2>nul
+net stop "Dnscache">nul 2>nul
+sc config Dnscache start= disabled>nul 2>nul
+ipv6 install>nul 2>nul
 echo 正在刷新dns缓存
 ipconfig /flushdns>nul 2>nul
 echo 正在清理IE缓存...
@@ -24,15 +27,15 @@ del /f /s /q "%userprofile%\recent\*.*">nul 2>nul
 del /f /q %userprofile%\recent\*.*>nul 2>nul
 ipconfig /flushdns>nul 2>nul
 echo 恢复IE搜索引擎,修复IE工具栏广告
-reg add "HKCU\Software\Microsoft\Internet Explorer\Main" /f /v "SearchUrl" /d "https://www.google.com/keyword/%s"
-reg add "HKCU\Software\Microsoft\Internet Explorer\Main" /v "Search Bar" /d "http://www.google.com/ie" /f
-reg add "HKCU\Software\Microsoft\Internet Explorer\Main" /v "Use Search Asst" /d "no" /f
-reg add "HKCU\Software\Microsoft\Internet Explorer\SearchURL" /v "provider" /d "gogl" /f
-reg add "HKLM\Software\Microsoft\Internet Explorer\" /f /ve /d "about:blank"
-reg add "HKLM\Software\Microsoft\Internet Explorer\Main" /f /v "Search Page" /d "https://www.google.com/intl/zh-CN/"
-reg add "HKLM\SOFTWARE\Microsoft\Internet Explorer\Search" /v "SearchAssistant" /d "https://www.google.com/ie" /f
-reg delete "HKLM\Software\Microsoft\Internet Explorer\Extensions" /f
-reg delete "HKCU\Software\Microsoft\Internet Explorer\Extensions" /f
+reg add "HKCU\Software\Microsoft\Internet Explorer\Main" /f /v "SearchUrl" /d "https://www.google.com/keyword/%s">nul 2>nul
+reg add "HKCU\Software\Microsoft\Internet Explorer\Main" /v "Search Bar" /d "http://www.google.com/ie" /f>nul 2>nul
+reg add "HKCU\Software\Microsoft\Internet Explorer\Main" /v "Use Search Asst" /d "no" /f>nul 2>nul
+reg add "HKCU\Software\Microsoft\Internet Explorer\SearchURL" /v "provider" /d "gogl" /f>nul 2>nul
+reg add "HKLM\Software\Microsoft\Internet Explorer\" /f /ve /d "about:blank">nul 2>nul
+reg add "HKLM\Software\Microsoft\Internet Explorer\Main" /f /v "Search Page" /d "https://www.google.com/intl/zh-CN/">nul 2>nul
+reg add "HKLM\SOFTWARE\Microsoft\Internet Explorer\Search" /v "SearchAssistant" /d "https://www.google.com/ie" /f>nul 2>nul
+reg delete "HKLM\Software\Microsoft\Internet Explorer\Extensions" /f>nul 2>nul
+reg delete "HKCU\Software\Microsoft\Internet Explorer\Extensions" /f>nul 2>nul
 cls
 echo 清除SOGOU搜狗的IE加载项so.dll
 regsvr32 /u /s so.dll >nul 2>nul
