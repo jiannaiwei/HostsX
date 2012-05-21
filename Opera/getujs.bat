bffd96ce55af9f236368f99548c3a69aee83bdd8
vokins <vokins@gmail.com>
2012-05-21 04:49:34 +0000

    更新opera数据
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@1631 2448481a-0d61-11df-8457-13bc90d6e451

--- Opera/getujs.bat
+++ Opera/getujs.bat
@@ -1,17 +1,17 @@
 @echo off
 pushd %~dp0
 del *.user.jsx
+del DBankLinker.js OnlinedownNoAds.user.js doubanfilm.user.js doubaniask.user.js doubanimdb.user.js TianyaRead.user.js CleanPlayer.user.js goglrwt.js
 wget http://userscripts.org/scripts/source/105184.user.js
 ren 105184.user.js angerOfPresident.user.jsx
 wget http://userscripts.org/scripts/source/120679.user.js
-ren 120679.user.js CleanPlayer.user.jsx
+ren 120679.user.js CleanPlayer.user.js
 wget http://userscripts.org/scripts/source/64877.user.js
 ren 64877.user.js Enhanced-word-highlight.user.jsx
 wget http://userscripts.org/scripts/source/88932.user.js
 ren 88932.user.js greasemonkey-emulation.user.jsx
 wget http://xiaonei-reformer.googlecode.com/files/xiaonei_reformer.min.user.js
 ren xiaonei_reformer.min.user.js xiaonei_reformer.min.user.jsx
-del DBankLinker.js OnlinedownNoAds.user.js doubanfilm.user.js doubaniask.user.js doubanimdb.user.js http2https.js goglrwt.js
 wget http://userscripts.org/scripts/source/103552.user.js
 ren 103552.user.js doubanimdb.user.js
 wget http://userscripts.org/scripts/source/116879.user.js
@@ -20,10 +20,10 @@ wget http://userscripts.org/scripts/source/129215.user.js
 ren 129215.user.js OnlinedownNoAds.user.js
 wget http://userscripts.org/scripts/source/129534.user.js
 ren 129534.user.js doubanfilm.user.js
-wget http://userscripts.org/scripts/source/2588.user.js
-ren 2588.user.js http2https.js
 wget http://userscripts.org/scripts/source/113577.user.js
 ren 113577.user.js goglrwt.js
 wget http://userscripts.org/scripts/source/91662.user.js
 ren 91662.user.js doubaniask.user.js
+wget http://userscripts.org/scripts/source/128239.user.js
+ren 128239.user.js TianyaRead.user.js
 msg %username% /time:3 "【UserJs数据已更新！】"
