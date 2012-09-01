578b3b54dba2861925effa574572a4f6c3418e7b
vokins <vokins@gmail.com>
2012-09-01 12:04:48 +0000

    修复youku等
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@1842 2448481a-0d61-11df-8457-13bc90d6e451

--- Windows/youku.bat
+++ Windows/youku.bat
@@ -1,13 +1,17 @@
 @echo off
-cd %HOMEPATH%\Application Data\Macromedia\Flash Player\#SharedObjects
+cd %APPDATA%\Macromedia\Flash Player\#SharedObjects
 DIR /AD /B >%TEMP%\dirname.txt
 for /f %%i in (%TEMP%\dirname.txt) do set a=%%i
 cd %a%
 attrib "static.youku.com" -r -h -s
 if exist "static.youku.com" (rd /q /s "static.youku.com") else (del /f /s /q "static.youku.com")
 @cd.>"static.youku.com"
-attrib +r static.youku.com
-start explorer.exe %HOMEPATH%\Application Data\Macromedia\Flash Player\#SharedObjects\%a%
+attrib +r "static.youku.com"
+attrib "irs01.net" -r -h -s
+if exist "irs01.net" (rd /q /s "irs01.net") else (del /f /s /q "irs01.net")
+@cd.>"irs01.net"
+attrib +r "irs01.net"
+start explorer.exe %APPDATA%\Macromedia\Flash Player\#SharedObjects\%a%
 del %TEMP%\dirname.txt
 echo ***YouKu广告屏蔽完成***
 goto:eof
