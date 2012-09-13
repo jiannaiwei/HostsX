b29db5744b9fe256d7b104ce7156c209a8e46886
vokins <vokins@gmail.com>
2012-09-13 13:35:52 +0000

    test new
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@1879 2448481a-0d61-11df-8457-13bc90d6e451

--- Windows/youku.bat
+++ Windows/youku.bat
@@ -9,4 +9,45 @@ c:> "%APPDATA%\Macromedia\Flash Player\#SharedObjects\!str!\irs01.net"
 rd "%APPDATA%\Macromedia\Flash Player\#SharedObjects\!str!\static.acs86.com" /s/q
 c:> "%APPDATA%\Macromedia\Flash Player\#SharedObjects\!str!\static.acs86.com")
 msg %username% /time:2 "优酷广告已免疫！"
-goto:EOF
+rd "%APPDATA%\Tencent\QQ\Misc\com.tencent.advertisement" /s/q
+c:> "%APPDATA%\Tencent\QQ\Misc\com.tencent.advertisement"
+rd "%APPDATA%\Tencent\QQ\SafeBase" /s/q
+c:> "%APPDATA%\Tencent\QQ\SafeBase"
+rd "%APPDATA%\Tencent\QQDoctor" /s/q
+c:> "%APPDATA%\Tencent\QQDoctor"
+rd "%APPDATA%\Tencent\QQ\Temp\gm" /s/q
+c:> "%APPDATA%\Tencent\QQ\Temp\gm"
+rd "%APPDATA%\Tencent\QQ\Misc\GMF" /s/q
+c:> "%APPDATA%\Tencent\QQ\Misc\GMF"
+rd "%APPDATA%\Tencent\IM" /s/q
+c:> "%APPDATA%\Tencent\IM"
+rd "%APPDATA%\Tencent\QQ\Misc\GTB_Icons" /s/q
+c:> "%APPDATA%\Tencent\QQ\Misc\GTB_Icons"
+rd "%APPDATA%\Tencent\QQ\Misc\GroupAlbumSnapshot" /s/q
+c:> "%APPDATA%\Tencent\QQ\Misc\GroupAlbumSnapshot"
+rd "%AllUsersProfile%\Application Data\Tencent\QQPCMgr" /s/q
+c:> "%AllUsersProfile%\Application Data\Tencent\QQPCMgr"
+msg %username% /time:2 "QQ广告已免疫！"
+rd "%AllUsersProfile%\Application Data\Windows Genuine Advantage" /s/q
+c:> "%AllUsersProfile%\Application Data\Windows Genuine Advantage"
+rd "%APPDATA%\PPLive\PPTV\Cache\pluginad" /s/q
+c:> "%APPDATA%\PPLive\PPTV\Cache\pluginad"
+rd "%AppData%\PPStream\adsys" /s/q
+c:> "%AppData%\PPStream\adsys"
+rd "%AppData%\PPStream\banner" /s/q
+c:> "%AppData%\PPStream\banner"
+rd "%AppData%\PPStream\Gmad" /s/q
+c:> "%AppData%\PPStream\Gmad"
+rd "%AppData%\PPStream\notice" /s/q
+c:> "%AppData%\PPStream\notice"
+rd "%AppData%\PPStream\TipXmls" /s/q
+c:> "%AppData%\PPStream\TipXmls"
+DEL /F /Q /A -R -H -S -A "%AllUsersProfile%\Application Data\Thunder Network\cid_store.dat"
+DEL /F /Q /A -R -H -S -A "%AllUsersProfile%\Application Data\Thunder Network\DownloadLib\pub_store.dat"
+Md "%AllUsersProfile%\Application Data\Thunder Network\cid_store.dat"
+Md "%AllUsersProfile%\Application Data\Thunder Network\emule_upload_list.dat"
+Md "%AllUsersProfile%\Application Data\Thunder Network\DownloadLib\pub_store.dat"
+attrib +r +s +h "%AllUsersProfile%\Application Data\Thunder Network\cid_store.dat"
+attrib +r +s +h "%AllUsersProfile%\Application Data\Thunder Network\emule_upload_list.dat"
+attrib +r +s +h "%AllUsersProfile%\Application Data\Thunder Network\DownloadLib\pub_store.dat"
+pause
