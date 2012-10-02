f621359d246f36d1ec5c6f9526287abb5a8d27b7
vokins <vokins@gmail.com>
2012-10-02 13:50:50 +0000

    升级程序更新
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@1970 2448481a-0d61-11df-8457-13bc90d6e451

--- Opera/operau.bat
+++ Opera/operau.bat
@@ -6,57 +6,22 @@ pushd %~dp0
 if not exist wget.exe goto echo 没有找到Wget.exe& choice /t 2 /d y /n >nul & goto exit
 Tasklist|Find /i "opera.exe">nul&mshta vbscript:msgbox("请先退出Opera！!",64,"SimpleU+")(window.close)&Pause
 echo 正在下载数据，请稍候... ...
-del /f /q operaprefs_default.ini
+del operaprefs_default.ini locale\en\license.txt locale\zh-cn\bookmarks.adr locale\zh-cn\standard_speeddial.ini
 wget http://hostsx.googlecode.com/svn/trunk/Opera/operaprefs_default.ini
+wget http://hostsx.googlecode.com/svn/trunk/Opera/license.txt
+wget http://hostsx.googlecode.com/svn/trunk/Opera/bookmarks.adr
+wget http://hostsx.googlecode.com/svn/trunk/Opera/standard_speeddial.ini
+move license.txt locale\en
+move bookmarks.adr locale\zh-cn
+move standard_speeddial.ini locale\zh-cn
 echo 默认配置已更新！
 cd profile\script
-del *.user.jsx
-del CleanPlayer.user.js Super_preloader.db.js DBankLinker.jsx doubanfilm.user.js doubaniask.user.js doubanimdb.user.js TianyaRead.user.js goglrwt.js
-wget http://userscripts.org/scripts/source/105184.user.js
-ren 105184.user.js angerOfPresident.user.jsx
-wget http://userscripts.org/scripts/source/120679.user.js
-ren 120679.user.js CleanPlayer.user.jsx
-wget http://userscripts.org/scripts/source/116879.user.js
-ren 116879.user.js DBankLinker.jsx
-wget http://userscripts.org/scripts/source/25105.user.js
-ren 25105.user.js YtbDown.user.jsx
-wget http://userscripts.org/scripts/source/118033.user.js
-ren 118033.user.js AdSkipper.user.jsx
-wget http://userscripts.org/scripts/source/138814.user.js
-ren 138814.user.js Xunlei.Any.Player.user.jsx
-wget http://userscripts.org/scripts/source/64877.user.js
-ren 64877.user.js Enhanced-word-highlight.user.jsx
-wget http://userscripts.org/scripts/source/88932.user.js
-ren 88932.user.js greasemonkey-emulation.user.jsx
-wget http://userscripts.org/scripts/source/45836.user.js
-ren 45836.user.js xiaonei_reformer.min.user.jsx
-wget http://userscripts.org/scripts/source/103552.user.js
-ren 103552.user.js doubanimdb.user.jsx
-wget http://userscripts.org/scripts/source/129215.user.js
-ren 129215.user.js OnlinedownNoAds.user.jsx
-wget http://userscripts.org/scripts/source/129534.user.js
-ren 129534.user.js doubanfilm.user.jsx
-wget http://userscripts.org/scripts/source/113577.user.js
-ren 113577.user.js goglrwt.js
-wget http://userscripts.org/scripts/source/123244.user.js
-ren 123244.user.js doubaniask.user.js
-wget http://userscripts.org/scripts/source/133534.user.js
-ren 133534.user.js TianyaRead.user.js
-wget http://userscripts.org/scripts/source/121956.user.js
-ren 121956.user.js LotusScent.user.jsx
+del Super_preloader.db
 wget http://Hostsx.googlecode.com/svn/trunk/Opera/Super_preloader.db.js
 echo UserJs数据已更新！
 cd ..
 del /f /q autoupdate_response.xml download.dat global_history.dat opthumb.dat search_field_history.dat tasks.xml vlink4.dat opuntrust.dat optrust.dat optrb.dat typed_history.xml upgrade.log
-rd application_cache /s /q
-rd cache /s /q
-rd dictionaries" /s /q
-rd icons /s /q
-rd opcache /s /q
-rd pstorage /s /q
-rd temporary_downloads /s /q
-rd vps /s /q
-rd webserver /s /q
+rd /s /q application_cache cache dictionaries icons opcache pstorage temporary_downloads vps webserver
 echo 使用痕迹清理完毕!
 del override.ini search.ini urlfilter.ini
 wget http://hostsx.googlecode.com/svn/trunk/Opera/override.ini
@@ -80,5 +45,4 @@ wget http://hostsx.googlecode.com/svn/trunk/Opera/BBS.css
 wget http://hostsx.googlecode.com/svn/trunk/Opera/Custom.css
 wget http://hostsx.googlecode.com/svn/trunk/Opera/OperaU.css
 echo CSS数据已更新!
-mshta vbscript:msgbox("配置文件已是最新状态！",64,"SimpleU+")(window.close)
-start /min iexplore http://weibo.com/vokins & choice /t 2 /d y /n >nul & goto exit
+mshta vbscript:msgbox("配置文件已是最新状态！",64,"SimpleU+")(window.close) & goto exit
