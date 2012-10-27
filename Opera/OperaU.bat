ae50951cb22adbf32e08435cfcedd98af2f5412d
vokins <vokins@gmail.com>
2012-10-27 15:58:01 +0000

    完善
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@2070 2448481a-0d61-11df-8457-13bc90d6e451

--- Opera/OperaU.bat
+++ Opera/OperaU.bat
@@ -1,14 +1,8 @@
 @echo off
-set ver=1.0.1.1
+set ver=1.0.1.2
 SetLocal EnableExtensions
 SetLocal EnableDelayedExpansion
 pushd %~dp0
-if not exist wget.exe goto echo 没有找到Wget.exe& choice /t 2 /d y /n >nul & goto exit
-Tasklist|Find /i "opera.exe">nul&mshta vbscript:msgbox("请先退出Opera！!",64,"SimpleU+")(window.close)&Pause
-rem 权限检测模块
-If "%PROCESSOR_ARCHITECTURE%"=="AMD64" (Set a=%SystemRoot%\SysWOW64) Else (Set a=%SystemRoot%\system32)
-Md "%a%\test_permissions" 2>nul||(echo 请使用右键-以管理员身份运行!!! & choice /t 2 /d y /n >nul &Exit)
-Rd "%a%\test_permissions" >nul 2>nul
 rem Opera参数调用设置
 if '%1'=='opis' goto:opinst
 if '%1'=='opup' goto:opupdate
@@ -16,6 +10,8 @@ if '%1'=='opcl' goto:opclean
 if '%1'=='opgdi' goto:opgdi
 
 :opupdate
+if not exist wget.exe goto echo 没有找到Wget.exe& choice /t 2 /d y /n >nul & goto exit
+Tasklist|Find /i "opera.exe">nul&mshta vbscript:msgbox("请先退出Opera！!",64,"SimpleU+")(window.close)&Pause
 echo 正在下载数据，请稍候... ...
 del operaprefs_default.ini
 wget http://hostsx.googlecode.com/svn/trunk/Opera/operaprefs_default.ini
@@ -60,6 +56,10 @@ echo 使用痕迹清理完毕!
 goto:eof
 
 :opinst
+rem 权限检测模块
+If "%PROCESSOR_ARCHITECTURE%"=="AMD64" (Set a=%SystemRoot%\SysWOW64) Else (Set a=%SystemRoot%\system32)
+Md "%a%\test_permissions" 2>nul||(echo 请使用右键-以管理员身份运行!!! & choice /t 2 /d y /n >nul &Exit)
+Rd "%a%\test_permissions" >nul 2>nul
 mshta vbscript:msgbox("请务必在安装使用前详细阅读服务条款中的内容和问题解答！！！",64,"SimpleU+ Opera")(window.close)
 opera.exe -install /desktopshortcut 1 /quicklaunchshortcut 0 /startmenushortcut 0 /setdefaultbrowser 0 /launchopera 1
 if not exist %a%\OperaJR.exe copy program\OperaJR.exe %a% >nul 2>nul
@@ -70,6 +70,15 @@ reg add "HKCR\operajr\shell\open\command" /v "Browser" /d "C:\Program Files\Inte
 reg add "HKCU\Software\Classes\operajr" /v "FriendlyTypeName" /d "opera" /f >nul 2>nul
 reg add "HKCU\Software\Classes\operajr" /v "URL Protocol" /d "" /f >nul 2>nul
 reg add "HKCU\Software\Classes\operajr\shell\open\command" /ve /d "\"C:\Windows\System32\OperaJR.exe\" \"%%1\"" /f >nul 2>nul
+for /f "delims=" %%i in ('dir /b /ad "%APPDATA%\Macromedia\Flash Player\#SharedObjects\"') do (
+set str=%%i
+rd "%APPDATA%\Macromedia\Flash Player\#SharedObjects\!str!\static.youku.com" /s/q
+c:> "%APPDATA%\Macromedia\Flash Player\#SharedObjects\!str!\static.youku.com"
+rd "%APPDATA%\Macromedia\Flash Player\#SharedObjects\!str!\irs01.net" /s/q
+c:> "%APPDATA%\Macromedia\Flash Player\#SharedObjects\!str!\irs01.net"
+rd "%APPDATA%\Macromedia\Flash Player\#SharedObjects\!str!\static.acs86.com" /s/q
+c:> "%APPDATA%\Macromedia\Flash Player\#SharedObjects\!str!\static.acs86.com")
+goto:EOF
 exit
 
 :opgdi
