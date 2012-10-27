a5ce66683e271efe46bd6fb5097a42d8cce0ec97
vokins <vokins@gmail.com>
2012-10-27 13:33:05 +0000

    独立opera相关的一些功能
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@2061 2448481a-0d61-11df-8457-13bc90d6e451

--- Opera/operau.bat
+++ Opera/operau.bat
@@ -1,10 +1,20 @@
 @echo off
-set ver=1.0.0.9
+set ver=1.0.1.1
 SetLocal EnableExtensions
 SetLocal EnableDelayedExpansion
 pushd %~dp0
 if not exist wget.exe goto echo 没有找到Wget.exe& choice /t 2 /d y /n >nul & goto exit
 Tasklist|Find /i "opera.exe">nul&mshta vbscript:msgbox("请先退出Opera！!",64,"SimpleU+")(window.close)&Pause
+rem 权限检测模块
+If "%PROCESSOR_ARCHITECTURE%"=="AMD64" (Set a=%SystemRoot%\SysWOW64) Else (Set a=%SystemRoot%\system32)
+Md "%a%\test_permissions" 2>nul||(echo 请使用右键-以管理员身份运行!!! & choice /t 2 /d y /n >nul &Exit)
+Rd "%a%\test_permissions" >nul 2>nul
+rem Opera参数调用设置
+if '%1'=='opis' goto:opinst
+if '%1'=='opup' goto:opupdate
+if '%1'=='opcl' goto:opclean
+
+:opupdate
 echo 正在下载数据，请稍候... ...
 del operaprefs_default.ini
 wget http://hostsx.googlecode.com/svn/trunk/Opera/operaprefs_default.ini
@@ -40,7 +50,24 @@ move /y standard_toolbar.ini profile\toolbar
 move /y BBS.css profile\styles\user
 move /y Custom.css profile\styles\user
 move /y OperaU.css profile\styles\user
+cls & echo 配置文件已是最新状态！& choice /t 2 /d y /n >nul & goto exit
+
+:opclean
 del /f /q autoupdate_response.xml download.dat global_history.dat search_field_history.dat tasks.xml vlink4.dat opuntrust.dat optrust.dat optrb.dat typed_history.xml upgrade.log
 rd /s /q application_cache cache dictionaries icons opcache pstorage temporary_downloads vps webserver
 echo 使用痕迹清理完毕!
-cls & echo 配置文件已是最新状态！& choice /t 2 /d y /n >nul & goto exit
+goto:eof
+
+:opinst
+mshta vbscript:msgbox("请务必在安装使用前详细阅读服务条款中的内容和问题解答！！！",64,"SimpleU+ Opera")(window.close)
+opera.exe -install /desktopshortcut 1 /quicklaunchshortcut 0 /startmenushortcut 0 /setdefaultbrowser 0 /launchopera 1
+if not exist %a%\OperaJR.exe copy program\OperaJR.exe %a% >nul 2>nul
+reg add "HKCR\operajr" /v "FriendlyTypeName" /d "opera" /f >nul 2>nul
+reg add "HKCR\operajr" /v "URL Protocol" /d "" /f >nul 2>nul
+reg add "HKCR\operajr\shell\open\command" /ve /d "\"C:\Windows\System32\OperaJR.exe\" \"%%1\"" /f >nul 2>nul
+reg add "HKCR\operajr\shell\open\command" /v "Browser" /d "C:\Program Files\Internet Explorer\iexplore.exe" /f >nul 2>nul
+reg add "HKCU\Software\Classes\operajr" /v "FriendlyTypeName" /d "opera" /f >nul 2>nul
+reg add "HKCU\Software\Classes\operajr" /v "URL Protocol" /d "" /f >nul 2>nul
+reg add "HKCU\Software\Classes\operajr\shell\open\command" /ve /d "\"C:\Windows\System32\OperaJR.exe\" \"%%1\"" /f >nul 2>nul
+exit
+
