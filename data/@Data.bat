6ce8c8b0b887ee2f2d89c7a05de9620ce7bec257
vokins <vokins@gmail.com>
2012-12-14 15:02:51 +0000

    test
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@2192 2448481a-0d61-11df-8457-13bc90d6e451

--- data/@Data.bat
+++ data/@Data.bat
@@ -2,12 +2,12 @@
 SetLocal EnableExtensions
 SetLocal EnableDelayedExpansion
 title Hosts数据合并更新工具
-echo.>temp.txt
+echo.>Version.txt
 echo ;version=%time% %date%>>Version.txt
 echo ;hostsxversion=0.5.2.1>>Version.txt
 echo ;author=KwokTree.jason_jiang.OrzFly.Felix Hsu.linjimmy.ZephyR>>Version.txt
 echo ;description=Clean Safe and Useful Hosts file.Thanks EveryOne.>>Version.txt
-set files=Version.txt Rd.txt apple.txt 1Key.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Soft.txt Mobile.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Popups.txt
+set files=Version.txt Rd.txt 1Key.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Soft.txt Mobile.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Popups.txt
 for %%a in (%files%) do (type "%%a">>HostsX.orzhosts)
 start hosts.vbs
 pause
