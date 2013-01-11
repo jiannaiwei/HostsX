92847d6e6fb8e7eb77f9d5eb94c480e4bcc96f18
vokins <vokins@gmail.com>
2013-01-11 13:17:13 +0000

    更新数据工具，更新组件，完善数据处理更新方式。完善op相关数据
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@2287 2448481a-0d61-11df-8457-13bc90d6e451

--- data/@Data.bat
+++ data/@Data.bat
@@ -1,8 +1,11 @@
 @echo off
 SetLocal EnableExtensions
 SetLocal EnableDelayedExpansion
-title Hosts数据合并更新工具
-echo.>Version.txt
+echo title HostsX 数据更新工具 >Version.txt
+echo more +5 %%~fs0^>%%systemroot%%\system32\drivers\etc\hosts >>Version.txt
+echo notepad %%windir%%\system32\drivers\etc\hosts >>Version.txt
+echo goto :eof >>Version.txt
+echo.>>Version.txt
 echo ;version=%time% %date%>>Version.txt
 echo ;hostsxversion=0.5.2.1>>Version.txt
 echo ;author=KwokTree.jason_jiang.OrzFly.Felix Hsu.linjimmy.ZephyR>>Version.txt
@@ -10,7 +13,7 @@ echo ;description=Clean Safe and Useful Hosts file.Thanks EveryOne.>>Version.txt
 set files=Version.txt Rd.txt 1Key.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Soft.txt Mobile.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Popups.txt
 for %%a in (%files%) do (type "%%a">>HostsX.orzhosts)
 start hosts.vbs
-pause
+ping -n 2 127.0.0.1
 move /y HostsX.orzhosts "%~dp0..\"
 move /y hosts "%~dp0..\"
 del /f Version.txt
