6ceb4ade80ef8f0ee28db5252baeaf43bd2ed507
vokins <vokins@gmail.com>
2013-01-22 08:14:26 +0000

    git-svn-id: https://hostsx.googlecode.com/svn/trunk@2337 2448481a-0d61-11df-8457-13bc90d6e451

--- Opera/getujs.bat
+++ Opera/getujs.bat
@@ -7,8 +7,6 @@ wget http://userscripts.org/scripts/source/116879.user.js
 ren 116879.user.js DBankLinker.js
 wget http://userscripts.org/scripts/source/126489.user.js
 ren 126489.user.js FlashBlock.js
-wget --no-check-certificate -N "https://raw.github.com/izml/ujs/master/ush.js"
-wget --no-check-certificate -N "https://raw.github.com/izml/ujs/master/InputCtrl.js"
 rem https://raw.bitbucket.org/zbinlin/blockpopupwindow/get/tip.zip
 wget http://userscripts.org/scripts/source/154323.user.js
 ren 154323.user.js SmoothScrollTopBottom.js
@@ -27,12 +25,12 @@ wget http://userscripts.org/scripts/source/118033.user.js
 ren 118033.user.js AdSkipper.user.js
 wget http://userscripts.org/scripts/source/138814.user.js
 ren 138814.user.js Xunlei.Any.Player.user.js
-wget http://userscripts.org/scripts/source/64877.user.js
-ren 64877.user.js Enhanced-word-highlight.user.js
 wget http://userscripts.org/scripts/source/105153.user.js
 ren 105153.user.js greasemonkey-emulation.user.js
 wget http://userscripts.org/scripts/source/45836.user.js
 ren 45836.user.js xiaonei_reformer.min.user.js
 wget http://userscripts.org/scripts/source/121956.user.js
 ren 121956.user.js LotusScent.user.js
+wget --no-check-certificate -N "https://raw.github.com/izml/ujs/master/ush.js"
+wget --no-check-certificate -N "https://raw.github.com/izml/ujs/master/InputCtrl.js"
 msg %username% /time:3 "【UserJs数据已更新！】"&exit
