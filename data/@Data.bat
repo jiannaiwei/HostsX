1a7dd9584c529f2c8d22c8ef98de8843246db935
vokins <vokins@gmail.com>
2013-01-23 07:16:38 +0000

    更新数据处理合并方式，更好的引用smarthosts的google有效数据.完善opera鼠标手势
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@2354 2448481a-0d61-11df-8457-13bc90d6e451

--- data/@Data.bat
+++ data/@Data.bat
@@ -5,16 +5,23 @@ echo title HostsX 数据更新工具 >bat.txt
 echo more +5 %%~fs0^>%%systemroot%%\system32\drivers\etc\hosts >>bat.txt
 echo notepad %%windir%%\system32\drivers\etc\hosts >>bat.txt
 echo goto :eof >>bat.txt
+wget http://smarthosts.googlecode.com/svn/trunk/hosts
+ren hosts 1.txt
+sed -i "1,7d" 1.txt
+sed -i "/Facebook End/,$d" 1.txt
+sed -i "/^$/d" 1.txt
+sed -i "/^#/d" 1.txt
+sed -i "1i\#Smarthosts-Google" 1.txt
 echo.>Version.txt
 echo ;version=%time% %date%>>Version.txt
 echo ;hostsxversion=0.5.2.1>>Version.txt
 echo ;author=KwokTree.jason_jiang.OrzFly.Felix Hsu.linjimmy.ZephyR>>Version.txt
 echo ;description=Clean Safe and Useful Hosts file.Thanks EveryOne.>>Version.txt
-set files=Version.txt Rd.txt 1Key.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Soft.txt Mobile.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Popups.txt
+set files=Version.txt Rd.txt 1.txt ggrd.txt 1Key.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Soft.txt Mobile.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Popups.txt
 for %%a in (%files%) do (type "%%a">>hbhosts.txt)
 start hosts.vbs
 ping -n 2 127.0.0.1
 copy /b bat.txt+hbhosts.txt HostsX.orzhosts
 move /y HostsX.orzhosts "%~dp0..\"
 move /y hosts "%~dp0..\"
-del /f Version.txt bat.txt hbhosts.txt
+del /f Version.txt bat.txt hbhosts.txt 1.txt
