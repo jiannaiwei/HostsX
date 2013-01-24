72fdcc499d48b7be4f9afdfc28b5946c64d414e7
vokins <vokins@gmail.com>
2013-01-24 05:59:34 +0000

    修复可能存在的目录错误
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@2370 2448481a-0d61-11df-8457-13bc90d6e451

--- data/@Data.bat
+++ data/@Data.bat
@@ -6,6 +6,7 @@ echo more +5 %%~fs0^>%%systemroot%%\system32\drivers\etc\hosts >>bat.txt
 echo notepad %%windir%%\system32\drivers\etc\hosts >>bat.txt
 echo goto :eof >>bat.txt
 wget http://smarthosts.googlecode.com/svn/trunk/hosts
+ping -n 5 127.0.0.1
 ren hosts 1.txt
 sed -i "1,7d" 1.txt
 sed -i "/Facebook End/,$d" 1.txt
@@ -17,7 +18,7 @@ echo ;version=%time% %date%>>Version.txt
 echo ;hostsxversion=0.5.2.1>>Version.txt
 echo ;author=KwokTree.jason_jiang.OrzFly.Felix Hsu.linjimmy.ZephyR>>Version.txt
 echo ;description=Clean Safe and Useful Hosts file.Thanks EveryOne.>>Version.txt
-set files=Version.txt Rd.txt 1.txt ggrd.txt 1Key.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Soft.txt Mobile.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Popups.txt
+set files=Version.txt Rd.txt 1.txt 1Key.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Game.txt Soft.txt Mobile.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Popups.txt
 for %%a in (%files%) do (type "%%a">>hbhosts.txt)
 start hosts.vbs
 ping -n 2 127.0.0.1
