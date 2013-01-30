75d28f25aae441bc616c12accf5bca666a0290ca
vokins <vokins@gmail.com>
2013-01-30 03:20:14 +0000

    测试flickr
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@2402 2448481a-0d61-11df-8457-13bc90d6e451

--- data/@Data.bat
+++ data/@Data.bat
@@ -18,7 +18,7 @@ echo ;version=%time% %date%>>Version.txt
 echo ;hostsxversion=0.5.2.1>>Version.txt
 echo ;author=KwokTree.jason_jiang.OrzFly.Felix Hsu.linjimmy.ZephyR>>Version.txt
 echo ;description=Clean Safe and Useful Hosts file.Thanks EveryOne.>>Version.txt
-set files=Version.txt Rd.txt 1.txt 1Key.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Game.txt Soft.txt Mobile.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Popups.txt
+set files=Version.txt Rd.txt 1.txt 1Key.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Game.txt Soft.txt Mobile.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt 360url.txt Popups.txt
 for %%a in (%files%) do (type "%%a">>hbhosts.txt)
 start hosts.vbs
 ping -n 2 127.0.0.1
