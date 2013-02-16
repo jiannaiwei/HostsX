b59c4cf32759eef02c411e84eb173b537efff478
vokins <vokins@gmail.com>
2013-02-16 15:16:23 +0000

    更新df相关内容
    git-svn-id: https://hostsx.googlecode.com/svn/trunk@2487 2448481a-0d61-11df-8457-13bc90d6e451

--- DNSForwarder/dnsforwarder.bat
+++ DNSForwarder/dnsforwarder.bat
@@ -4,13 +4,22 @@ sc stop dnsforwarder
 sc delete dnsforwarder 
 sc create DNSForwarder binPath= "%cd%\srvany.exe" start= auto
 sc description dnsforwarder "UDP to TCP DNS Forwarder"
-reg add HKLM\SYSTEM\CurrentControlSet\Services\dnsforwarder\Parameters /v Application /d "%cd%\dnsforwarder.exe" /f
+rem 权限检测模块,判断操作系统版本
+If "%PROCESSOR_ARCHITECTURE%"=="AMD64" (call :lnk64) Else (call :lnk32)
 reg add HKLM\SYSTEM\CurrentControlSet\Services\dnsforwarder\Parameters /v AppDirectory /d "%cd%" /f
 sc start dnsforwarder
 @echo 成功了～！
 @echo.
 @pause
 exit
+
+:lnk32
+reg add HKLM\SYSTEM\CurrentControlSet\Services\dnsforwarder\Parameters /v Application /d "%cd%\dnsforwarder_32.exe" /f
+goto :eof
+
+:lnk64
+reg add HKLM\SYSTEM\CurrentControlSet\Services\dnsforwarder\Parameters /v Application /d "%cd%\dnsforwarder_64.exe" /f
+goto :eof
 
 !delete_dnsproxy_server.bat
 sc stop dnsforwarder 
