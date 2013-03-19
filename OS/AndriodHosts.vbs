9d0e8af8dd559cc1889aa804067ac31c1fa17e2b
vokins <vokins@gmail.com>
2013-03-19 23:51:55 +0800

    大规模调整数据架构。改变以系统划分的目录，统一调整系统数据部分到OS里面，删除不必要的浏览器配置等数据，以后会有simpleu配置opera的git

---
+++ OS/AndriodHosts.vbs
@@ -0,0 +1,78 @@
+function GetHttpFile(url) 
+    dim m,s 
+m="Microsoft.XMLHTTP" 
+s="ADODB.Stream" 
+    FileName=LCase(mid(url,InStrRev(url, "/")+1)) 
+Set xml = CreateObject(m) 
+xml.Open "GET",url,False 
+xml.Send 
+with CreateObject(s) 
+.type=1 
+.open 
+.write xml.responseBody 
+.savetofile ".\"&FileName,2 
+end with 
+set xml=Nothing 
+end function
+GetHttpFile("http://hostsx.googlecode.com/svn/trunk/HostsX.orzhosts")
+GetHttpFile("http://hostsx.googlecode.com/svn/trunk/Android/SDCard.zip")
+GetHttpFile("http://hostsx.googlecode.com/svn/trunk/Android/adb.exe")
+GetHttpFile("http://hostsx.googlecode.com/svn/trunk/Android/AdbWinApi.dll")
+GetHttpFile("http://hostsx.googlecode.com/svn/trunk/Android/AdbWinUsbApi.dll")
+'------------------------------------------------------------
+set fso=createobject("scripting.filesystemobject")
+set file=fso.opentextfile("HostsX.orzhosts")
+s=file.readall
+file.close
+s=replace(s,"0.0.0.0","127.0.0.1")
+set file=fso.createtextfile("new.txt")
+file.write s
+file.close
+Main
+Sub Main()
+    strSrcFile = ".\new.txt"
+    strDestFile = ".\hosts"
+    strDos = ReadFileToString(strSrcFile)
+    strUnix = ConvDosToUnix(strDos)
+    WriteStringToFile strDestFile, strUnix
+End Sub
+Function ConvDosToUnix(strInfo)
+    ConvDosToUnix = Replace(strInfo, vbCrLf, vbLf)
+End Function
+Function ReadFileToString(strFilePath)
+    Const ForReading = 1
+    Set ofs = CreateObject("Scripting.FileSystemObject")
+    Set ofile = ofs.OpenTextFile(strFilePath, ForReading, True)
+    ReadFileToString = ofile.ReadAll
+    ofile.Close
+End Function
+Sub WriteStringToFile(strFilePath, strInfo)
+    Const ForWriting = 2
+    Const isCreateNew = True
+    Set ofs = CreateObject("Scripting.FileSystemObject")
+    Set ofile = ofs.OpenTextFile(strFilePath, ForWriting, isCreateNew)
+    ofile.Write strInfo
+    ofile.Close
+End Sub
+'------------------------------------------------------------
+MsgBox "Hosts Data downloaded and conversion!", vbInformation, "HostsX"
+MsgBox "Please send your phone connected to the computer£¬and open the USB debug mode!", vbInformation, "HostsX"
+set fso=createobject("scripting.filesystemobject")
+set ws=CreateObject("wscript.shell")
+set fw=fso.createtextfile("transfer.bat",2)
+fw.writeline("@echo on")
+fw.writeline("color 0b")
+fw.writeline("adb devices")
+fw.writeline("adb root")
+fw.writeline("adb remount")
+fw.writeline("adb push hosts /etc/hosts")
+fw.writeline("adb shell chmod 644 /etc/hosts")
+fw.writeline("adb shell reboot")
+fw.writeline("echo Hosts complete Update!")
+fw.writeline("echo Enjoy it!")
+fw.writeline("taskkill  /f /T /im adb.exe")
+fw.writeline("pause")
+fw.writeline("del /f /q hosts adb.exe adbwinapi.dll adbwinusbapi.dll >nul 2>nul")
+fw.writeline("del %0")
+fw.close
+ws.run "transfer.bat"
--- data/@Data.bat
+++ data/@Data.bat
@@ -1,14 +1,45 @@
 @echo off
 SetLocal EnableExtensions
 SetLocal EnableDelayedExpansion
-del /f Android.txt out.txt Version.txt bat.txt hbhosts.txt 1.txt hosts.txt Applenew.txt 1hosts.txt unix.txt out.txt
+del /f Android.txt out.txt Version.txt bat.txt hbhosts.txt 1.txt hosts.txt Applenew.txt 1hosts.txt unix.txt out.txt hosts
 rem http://sourceforge.net/projects/dos2unix/
 mac2unix -ascii -n Apple.txt out.txt
 unix2dos -n out.txt Applenew.txt 
-echo title HostsX 数据更新工具 >bat.txt
+echo title HostsX BatchVersion >bat.txt
 echo more +5 %%~fs0^>%%systemroot%%\system32\drivers\etc\hosts >>bat.txt
 echo notepad %%windir%%\system32\drivers\etc\hosts >>bat.txt
 echo goto :eof >>bat.txt
+rem :smarhosts
+echo.>Version.txt
+echo ;version=%date:~0,4%%date:~5,2%%date:~8,2%%TIME:~0,2%%TIME:~3,2%>>Version.txt
+rem echo ;version=%time% %date%>>Version.txt
+echo ;hostsxversion=0.5.2.1>>Version.txt
+echo ;author=kwoktree.OrzFly.jason_jiang.Felix Hsu.linjimmy.ZephyR.atmouse.Ehosts.zhqjsh>>Version.txt
+echo ;description=Clean Safe and Useful Hosts file.Thanks EveryOne.>>Version.txt
+set files=bat.txt Version.txt Rd.txt 1Key.txt Mobile.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Game.txt Soft.txt UnionEN.txt UnionRU.txt UnionJP.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Mwsl.txt Popups.txt
+for %%a in (%files%) do (type "%%a">>HostsX.orzhosts)
+copy HostsX.orzhosts Android.txt
+start hosts.vbs
+ping -n 2 127.0.0.1
+move /y hosts "%~dp0..\Android"
+del new.txt Android.txt
+move /y HostsX.orzhosts "%~dp0..\"
+set files=Version.txt Rd.txt 1Key.txt Mobile.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Game.txt Soft.txt UnionEN.txt UnionRU.txt UnionJP.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Mwsl.txt Popups.txt
+for %%a in (%files%) do (type "%%a">>Android.txt)
+start hosts.vbs
+ping -n 2 127.0.0.1
+md "%~dp0..\OS\system\etc"
+copy hosts "%~dp0..\OS\system\etc"
+copy hosts "%~dp0..\"
+del /f Android.txt out.txt Version.txt bat.txt hbhosts.txt 1.txt hosts.txt Applenew.txt 1hosts.txt unix.txt out.txt hosts
+echo 7z u HostsX_updates.zip system\etc\hosts >"%~dp0..\OS\7z.bat"
+echo rd /s/q system >>"%~dp0..\OS\7z.bat"
+echo del %%0 >>"%~dp0..\OS\7z.bat"
+cd %~dp0..\OS
+7z.bat
+exit
+
+:smarhosts
 wget http://smarthosts.googlecode.com/svn/trunk/hosts
 ping -n 5 127.0.0.1
 ren hosts 1.txt
@@ -16,31 +47,16 @@ sed -i "1,7d" 1.txt
 sed -i "/Facebook End/,$d" 1.txt
 sed -i "/^$/d" 1.txt
 sed -i "/^#/d" 1.txt
+sed -i "/googlesyndication.com/d" 1.txt
+sed -i "/google-analytics.com/d" 1.txt
+sed -i "/googleadservices.com/d" 1.txt
 ping -n 3 127.0.0.1
 sed -i "1i\#Smarthosts-Google" 1.txt
-echo.>Version.txt
-echo ;version=%time% %date%>>Version.txt
-echo ;hostsxversion=0.5.2.1>>Version.txt
-echo ;author=KwokTree.jason_jiang.OrzFly.Felix Hsu.linjimmy.ZephyR>>Version.txt
-echo ;description=Clean Safe and Useful Hosts file.Thanks EveryOne.>>Version.txt
-set files=bat.txt Version.txt Rd.txt 1.txt 1Key.txt Mobile.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Game.txt Soft.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt 360url.txt Popups.txt
-for %%a in (%files%) do (type "%%a">>HostsX.orzhosts)
-move /y HostsX.orzhosts "%~dp0..\"
-set files=Version.txt Rd.txt Applenew.txt 1Key.txt Mobile.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Game.txt Soft.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Popups.txt
+goto :eof
+
+:mac
+set files=Version.txt Rd.txt Applenew.txt 1Key.txt Mobile.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Game.txt Soft.txt UnionEN.txt UnionRU.txt UnionJP.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Popups.txt
 for %%a in (%files%) do (type "%%a">>hosts.txt)
 sed -e "s/0.0.0.0/127.0.0.1/g" hosts.txt > hosts
 move /y hosts "%~dp0..\"
-set files=Version.txt Rd.txt 1Key.txt Mobile.txt SiteEN.txt SiteCN.txt Media.txt Active.txt Game.txt Soft.txt UnionEN.txt UnionCN.txt Dnt.txt Hijack.txt HijackIP.txt Virus.txt Popups.txt
-for %%a in (%files%) do (type "%%a">>Android.txt)
-start hosts.vbs
-ping -n 2 127.0.0.1
-del /f Android.txt Version.txt bat.txt hbhosts.txt 1.txt hosts.txt Applenew.txt 1hosts.txt unix.txt out.txt
-md "%~dp0..\Android\system\etc"
-copy hosts "%~dp0..\Android\system\etc"
-move /y hosts "%~dp0..\Android"
-echo 7z u HostsX_updates.zip system\etc\hosts >"%~dp0..\Android\7z.bat"
-echo rd /s/q system >>"%~dp0..\Android\7z.bat"
-echo del %%0 >>"%~dp0..\Android\7z.bat"
-cd %~dp0..\Android
-7z.bat
-exit
+goto :eof
