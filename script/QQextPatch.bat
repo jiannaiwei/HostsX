9da727acc0b94f891fb80f1ae33c0da6cacb9174
vokins <vokins@gmail.com>
2013-04-22 17:11:47 +0800

    QQextPatch.bat

---
+++ script/QQextPatch.bat
@@ -0,0 +1,3 @@
+DEL /F /Q /A -R -H -S -A "%appdata%\Tencent\QQ\SafeBase\tssafeedit.dat"
+Md "%appdata%\Tencent\QQ\SafeBase\tssafeedit.dat"
+attrib +r +s +h "%appdata%\Tencent\QQ\SafeBase\tssafeedit.dat"
